ec63a986f20f99db9320e2915002400f851c6c4e
richinelle <richinellelaurence@gmail.com>
2026-05-13 11:18:01 +0100

    feat(core): ajout du log de démarrage de l'application
    fix(cours): correction du titre du cours JavaScript
    
    feat(exercices): ajout des exercices pratiques Python
    
    WIP commit 1
    
    WIP commit 2
    
    feat(exos): ajout des exercices Python

--- cours.js
+++ cours.js
@@ -1,6 +1,6 @@
 // Module de gestion des cours
 const cours = [
  { id: 1, titre: 'Introduction à Python', niveau: 'débutant' },
- { id: 2, titre: 'JavaScript ES6+', niveau: 'intermédiaire' }
+ { id: 2, titre:  'JavaScript ES6+ & ES2022', niveau: 'intermédiaire' }
 ];
 module.exports = cours;
